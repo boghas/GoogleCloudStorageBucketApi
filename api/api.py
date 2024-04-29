eeec2059c54a7ba35e487b3ae18112d03988da93
boghas <bogdan.hasan@mindit.io>
2024-04-29 14:29:38 +0300

    Updated the code and added download feature

---
+++ api/api.py
@@ -0,0 +1,36 @@
+import os
+from google.cloud import storage
+from google.cloud.exceptions import NotFound
+from google.cloud.exceptions import Forbidden
+#from google.oauth2 import service_account
+
+
+
+def load_credentials():
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'credentials\duag-masc-stg-01-ops.json'
+
+
+def list_bucket_files(storage_client: storage.Client, bucket_name: str, directory=None | str) -> list:
+    blobs = storage_client.list_blobs(bucket_name)
+    if directory is not None:
+        file_names = [file.name for file in blobs if directory in file.name]
+    else:
+        file_names = [file.name for file in blobs]
+
+    return file_names
+
+
+def download_blobs(storage_client: storage.Client, bucket_name: str, blobs_list: list, local_download_dir='') -> None:
+    try:
+        bucket = storage_client.get_bucket(bucket_name)
+
+        for blob in blobs_list:
+            blob = bucket.blob(blob)
+            blob.download_to_filename(os.path.join(local_download_dir, os.path.basename(blob.name)))
+            print(f'Successfully downloaded {blob.name} to {os.path.join(local_download_dir, os.path.basename(blob.name))}')
+    except NotFound:
+        print('Bucket not found!')
+    except Forbidden:
+        print('You do not have permissions to perform this action!')
+
+
--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import os
 import shutil
 import logging
 from dotenv import load_dotenv
-from smth import smth
+from api import api
+from google.cloud import storage
+
 
 if __name__ == "__main__":
     # This loads the environment variables from the .env file
-    load_dotenv()  
+    load_dotenv()
+    GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")  
     bucket_name = os.environ.get('BUCKET_NAME')
+    bucket_dir = os.environ.get('BUCKET_DIR')
+    local_download_path = os.environ.get('LOCAL_DIR')
 
-    smth.load_credentials()
+    # Setup the client
+    storage_client = storage.Client()
+    
+    files = api.list_bucket_files(storage_client, bucket_name, bucket_dir)
+    print(files)
 
-    print(smth.list_bucket_files(bucket_name))
+    api.download_blobs(storage_client, bucket_name, files, local_download_path)
 
